710b83f8c7e0a95bd005ff11d2cd532b36c525b1
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-02-22 03:09:06 +0100

    finiched .. need to check norm tho

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 21:51:38 by agaladi           #+#    #+#             */
-/*   Updated: 2024/02/21 22:25:21 by agaladi          ###   ########.fr       */
+/*   Updated: 2024/02/22 02:58:19 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,13 @@ int main(void)
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
 	ft_putstr(TITLE_MSG);
-	ft_putstr("server PID: ");
-	ft_putstr(DEVIDER);
+	ft_putstr(V_DEVIDER);
+	ft_putstr("\t\e[1;97m\e[4;32mserver PID :");
+	ft_putstr(H_DEVIDER);
+	ft_putstr("\e[1;37m\e[42m");
 	ft_putstr(pid);
-	ft_putstr("\n");
+	ft_putstr(V_DEVIDER);
+	ft_putstr("\e[1;32m֎ \e[1;37mmessage\e[0;33m ⤳  \033[1;0m");
 
 	while (1)
 		sleep(1);
